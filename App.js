5fb5dc999c418b92057c22fafb8bb154e65e8ab3
Samitha Dilshan <it20607678@my.sliit.lk>
2022-11-01 17:03:26 +0530

    project init with routers

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from 'react-navigation-stack';
+
+
+
+import HomeScreen from './components/Homescreen';
+import ContactScreen from './components/ContactScreen';
+import AboutScreen from './components/Aboutscreen';
+
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  About: {
+    screen: AboutScreen
+  },
+  Contact: {
+    screen: ContactScreen
+  }
+}, {
+  initialRouteName: "Home"
+});
+
+
+
+const AppContainer = createAppContainer(AppNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Homescreen.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import { Button, View, Text, StyleSheet , Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import { Card } from 'react-native-paper';
+
+export default class Homescreen extends Component {
+  render() {
+    return (
+        <View>
+    <View>
+        <Text>Home Screen</Text>
+          <Button
+          title="Go to About"
+          onPress={() => this.props.navigation.navigate('About')}
+/>
+      </View>
+
+<View style={styles.container}>
+<View style={styles.card_template}>
+ <Image 
+   
+   style={styles.card_image}
+   source={{uri: 'https://i.picsum.photos/id/881/700/700.jpg?hmac=-JqTJ4_Ped2jYmjiaDgYZOAGzvC0CybCKbROT3GJgZc'}}
+/>
+<View style={styles.text_container}>
+ <Text style={styles.card_title}>Home Screen</Text>
+ <Button
+          title="Go to About"
+          onPress={() => this.props.navigation.navigate('About')}
+/>
+</View>
+</View>
+</View>
+
+
+
+</View>
+
+
+    )
+  }
+}
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      marginTop:100,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    card_template:{
+      width: 250,
+      height: 250,
+      boxShadow: "10px 10px 17px -12px rgba(0,0,0,0.75)"
+    },
+    card_image: {
+      width: 250,
+      height: 250,
+      borderRadius : 10
+    },
+    text_container:{
+      position: "absolute",
+      width: 250,
+      height: 30,
+      bottom:0,
+      padding: 5,
+      backgroundColor: "rgba(0,0,0, 0.3)",
+      borderBottomLeftRadius : 10,
+      borderBottomRightRadius: 10
+    },
+    card_title: {
+       color: "white",
+  
+    },
+  });
+  
